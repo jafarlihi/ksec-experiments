ebcb9d45869b3e39ec4adc9dca40e6a5c0bdc3b4
Hikmat Jafarli <jafarlihi@gmail.com>
2022-06-26 17:03:38 +0400

    add stuff

--- idt/hello.c
+++ idt/hello.c
@@ -6,16 +6,36 @@ typedef struct {
     unsigned __int128 offset;
 } __attribute__((packed)) idtr_t;
 
+typedef struct {
+  u16 offset_0_15;
+  u16 selector;
+  struct access_byte {
+    u8 ist : 2;
+    u8 reserved : 4;
+    u8 gate_type : 3;
+    u8 zero : 1;
+    u8 dpl : 1;
+    u8 p : 1;
+  } ab;
+  u16 offset_16_31;
+  u32 offset_32_63;
+  u32 reserved;
+} idt_entry_t;
+
+typedef union {
+  unsigned __int128 scalar;
+  idt_entry_t structure;
+} idt_entry_u_t;
+
 int init_module(void) {
     idtr_t idtr;
     asm("sidt %0" : "=m"(idtr));
 
     int i;
     for (i = 0; i <= idtr.size / 16; i++) {
-      unsigned __int128 entry = *((unsigned __int128 *)idtr.offset + i);
-      if (entry != 0) {
-        printk("Entry %d: 0x%llx%llx\n", i, entry, entry >> 64);
-      }
+      idt_entry_u_t entry;
+      entry.scalar = *((unsigned __int128 *)idtr.offset + i);
+      printk("Entry %d: 0x%llx%llx\n", i, (u64)entry.scalar, (u64)(entry.scalar >> 64));
     }
     return 0;
 }
